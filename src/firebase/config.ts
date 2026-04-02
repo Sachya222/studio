9821a5c56c5afd76cf565ac52e8f2c5a31db61a1
Infinity Work <sy155733@gmail.com>
2026-04-02 20:37:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8532427439-2f8ce",
+  "appId": "1:278191481040:web:3145b4034d3648a5e97ed5",
+  "apiKey": "AIzaSyBGFNP3VsBYByJwyOr4IbsWCRLGFFCayCg",
+  "authDomain": "studio-8532427439-2f8ce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "278191481040"
+};
